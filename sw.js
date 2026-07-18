7d5f26c8427e9e816fdd5693a60580e678cf03cd
arturoricome-ship-it <arturoricome@gmail.com>
2026-07-18 17:00:15 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-/* HERRAMIENTAS SW V64 - 2026-07-18 */
-const CACHE='herramientas-turnos-v64';
+/* HERRAMIENTAS SW V65 - PERENTORIAS CTA ORIGINAL - 2026-07-18 */
+const CACHE='herramientas-turnos-v65';
 const CORE=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>{
   self.skipWaiting();
@@ -10,15 +10,19 @@ self.addEventListener('activate',event=>{
 });
 self.addEventListener('fetch',event=>{
   if(event.request.method!=='GET')return;
-  const isNav=event.request.mode==='navigate';
-  if(isNav){
+  if(event.request.mode==='navigate'){
     event.respondWith(fetch(event.request,{cache:'no-store'}).then(response=>{
-      const copy=response.clone();caches.open(CACHE).then(cache=>cache.put('./index.html',copy));return response;
+      const copy=response.clone();
+      caches.open(CACHE).then(cache=>cache.put('./index.html',copy));
+      return response;
     }).catch(()=>caches.match('./index.html').then(r=>r||caches.match('./'))));
     return;
   }
-  event.respondWith(fetch(event.request).then(response=>{
-    if(response&&response.ok){const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));}
+  event.respondWith(fetch(event.request,{cache:'no-cache'}).then(response=>{
+    if(response&&response.ok){
+      const copy=response.clone();
+      caches.open(CACHE).then(cache=>cache.put(event.request,copy));
+    }
     return response;
   }).catch(()=>caches.match(event.request)));
 });
